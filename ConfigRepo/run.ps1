b62648827e800b7684a2f515887f1f299cb50004
Max Yermakhanov <max@automagically.io>
2023-05-10 14:08:35 +0300

    application/vnd.github+json

--- ConfigRepo/run.ps1
+++ ConfigRepo/run.ps1
@@ -20,9 +20,9 @@ Write-Host "Protected branch name:" $Request.rule.name
 # Header for GitHub API
 $ghToken = $env:ghToken
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Accept", "application/vnd.github.v3+json")
+$headers.Add("Accept", "application/vnd.github+json")
 $headers.Add("Authorization", "Basic $ghToken")
-$headers.Add("Content-Type", "application/json")
+$headers.Add("ContentType", "application/json")
 
 $ghRepoName = $Request.repository.name
 
@@ -44,6 +44,15 @@ function ConfigureBranchProtection {
     
     $response = Invoke-RestMethod "https://api.github.com/repos/$orgName/$ghRepoName/branches/$protectedBranch/protection" -Method 'PUT' -Headers $headers -Body $bodyConfigureProtection
     $response | ConvertTo-Json
+# curl -L \
+#   -X PUT \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer <YOUR-TOKEN>"\
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/repos/OWNER/REPO/branches/BRANCH/protection \
+#   -d '{"required_status_checks":{"strict":true,"contexts":["continuous-integration/travis-ci"]},"enforce_admins":true,"required_pull_request_reviews":{"dismissal_restrictions":{"users":["octocat"],"teams":["justice-league"]},"dismiss_stale_reviews":true,"require_code_owner_reviews":true,"required_approving_review_count":2,"require_last_push_approval":true,"bypass_pull_request_allowances":{"users":["octocat"],"teams":["justice-league"]}},"restrictions":{"users":["octocat"],"teams":["justice-league"],"apps":["super-ci"]},"required_linear_history":true,"allow_force_pushes":true,"allow_deletions":true,"block_creations":true,"required_conversation_resolution":true,"lock_branch":true,"allow_fork_syncing":true}'
+# https://api.github.com/repos/automagicallyorg/AppDeploy-Azure/branches/main/protection
+# https://github.com/automagicallyorg/AppDeploy-Azure/settings/branches
 }
     
 function AddReadMe {
